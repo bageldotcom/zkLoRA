74c567eaebaad354d7d1889a1202c8d197e23f17
Marcos Villagra <mdvillagra@gmail.com>
2025-09-15 19:49:24 -0300

    Add ONNX export functionality for LoRA submodules and implement activation capturing. Introduce `export_lora_submodules` function to facilitate the export of LoRA parameters and their activations to ONNX and JSON formats. Update `__init__.py` to include new exports, enhancing the module's capabilities for model integration.

--- src/zklora/__init__.py
+++ src/zklora/__init__.py
@@ -1,17 +1,23 @@
 __version__ = "0.1.2"
 
 from .zk_proof_generator import batch_verify_proofs
+from .zk_proof_generator import generate_proofs
+from .mpi_lora_onnx_exporter import export_lora_onnx_json_mpi
 from .lora_contributor_mpi import LoRAServer, LoRAServerSocket
 from .base_model_user_mpi import BaseModelClient
 from .polynomial_commit import commit_activations, verify_commitment
+from .lora_onnx_exporter import export_lora_submodules
 
 
 __all__ = [
-    "batch_verify_proofs",
-    "LoRAServer",
-    "LoRAServerSocket",
-    "BaseModelClient",
-    "commit_activations",
-    "verify_commitment",
-    "__version__",
+    'batch_verify_proofs',
+    'generate_proofs',
+    'export_lora_onnx_json_mpi',    
+    'export_lora_submodules',
+    'LoRAServer',
+    'LoRAServerSocket',
+    'BaseModelClient',
+    'commit_activations',
+    'verify_commitment',
+    '__version__',
 ]
